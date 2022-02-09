034cb3451063bd1dde47708c6abf89cf8bf741fc
kemal9191 <kpehlivan91@gmail.com>
2022-02-09 21:42:39 +0300

    third commit

--- scraper.py
+++ scraper.py
@@ -5,24 +5,38 @@ import requests
 
 df = pd.read_excel("links.xlsx")
 
-links= [] 
+items= [] 
 for index, row in df.iterrows(): 
     mylist = [row.url, row.title_tag, row.title_class, row.content_tag, row.content_class]
-    links.append(mylist)
+    items.append(mylist)
 
+titles = []
+contents = []
 
-for link in links:
-    url = link[0]
-    title_tag = link[1]
-    title_class_name = link[2]
-    content_tag = link[3]
-    content_class_name = link[4]
+for item in items:
+    url = item[0]
+    title_tag = item[1]
+    title_class_name = item[2]
+    content_tag = item[3]
+    content_class_name = item[4]
 
     try:
         html_content = requests.get(url).content
         soup = BeautifulSoup(html_content, "lxml")         
-        article_title = soup.find(title_tag, class_=title_class_name)
-        article_content = soup.find(content_tag, class_=content_class_name)
-        print(article_title.text, article_content.text)
+        article_title = soup.find(title_tag, class_=title_class_name).text
+        article_content = soup.find(content_tag, class_=content_class_name).text
+        titles.append(article_title)
+        contents.append(article_content)
     except Exception:
         print(sys.exc_info())
+
+df_results = pd.DataFrame({
+    'Title': [title for title in titles],
+    'Content': [content for content in contents]
+})
+
+writer = pd.ExcelWriter("results.xlsx")
+df_results.to_excel(writer)
+writer.save()
+print(titles)
+print([title for title in titles])
