7bf12b708cd8461a943703fef9dcfe34d29f698f
kemal9191 <67286816+kemal9191@users.noreply.github.com>
2022-02-10 17:09:17 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,26 @@
+from base64 import encode
+import encodings
+from json import encoder
 import sys
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+'''
+This is a small program that takes a file named "links" and 
+iterate through URLs inside it.links has 5 columns, namely url, 
+title_tag, title_class, content_tag, and content_class. Program 
+takes this information, goes to the url, finds the article 
+specified, then writes it to an excel file called results.xlsx. 
+This was designed to fetch articles from a blog site.
+'''
+
 df = pd.read_excel("links.xlsx")
 
 items= [] 
 for index, row in df.iterrows(): 
-    mylist = [row.url, row.title_tag, row.title_class, row.content_tag, row.content_class]
-    items.append(mylist)
+    item = [row.url, row.title_tag, row.title_class, row.content_tag, row.content_class]
+    items.append(item)
 
 titles = []
 contents = []
@@ -23,20 +35,17 @@ for item in items:
     try:
         html_content = requests.get(url).content
         soup = BeautifulSoup(html_content, "lxml")         
-        article_title = soup.find(title_tag, class_=title_class_name).text
-        article_content = soup.find(content_tag, class_=content_class_name).text
+        article_title = soup.find(title_tag, class_=title_class_name).text.replace("\n", "")
+        article_content = soup.find(content_tag, class_=content_class_name).text.replace("\n", "")
         titles.append(article_title)
         contents.append(article_content)
     except Exception:
         print(sys.exc_info())
 
 df_results = pd.DataFrame({
-    'Title': [title for title in titles],
-    'Content': [content for content in contents]
+    'Title':titles,
+    'Content':  contents
 })
 
-writer = pd.ExcelWriter("results.xlsx")
-df_results.to_excel(writer)
-writer.save()
-print(titles)
-print([title for title in titles])
+with pd.ExcelWriter("results.xlsx") as writer:
+    df_results.to_excel(writer, encoding="utf-8")
